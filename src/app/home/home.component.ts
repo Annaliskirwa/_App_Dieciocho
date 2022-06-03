a33258ddef3660117d94c4c7027057d3ce6f2928
Annaliskirwa <annaliskirwa@gmail.com>
2022-06-03 13:02:08 +0300

    changed to scss, styled the page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,10 +4,10 @@ import { ApiService } from "../services/api.service"
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  
+
   cats: any[] = []
 
   constructor(
