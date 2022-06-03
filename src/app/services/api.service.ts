9493bfad11e57b94e077549f05672ab4995ba3ee
Annaliskirwa <annaliskirwa@gmail.com>
2022-06-03 11:29:52 +0300

    Importing environment, base url

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  baseUrl = environment.API_ENDPOINT;
 
   constructor(
     private httpClient:HttpClient
